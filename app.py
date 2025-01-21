0c4bbef4b53776eb0babec09b5d57e49b892dc7f
esssorokinaaa <kattterinasorokina@gmail.com>
2025-01-21 16:29:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -156,35 +156,6 @@ def statistics():
 
    return send_file(img ,mimetype='image.png')
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-     form= RegisterForm()
-     if form.validate_on_submit():
-         new_user= User(username=form.username.data,password=form.password.data) 
-         new_user.password= generate_password_hash(form.password.data) 
-         db.session.add(new_user) 
-         db.session.commit() 
-         return redirect(url_for('login')) 
-
-     return render_template('register.html' ,form=form )
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-     form= LoginForm()
-     if form.validate_on_submit():
-         user= User.query.filter_by(username=form.username.data).first() 
-         if user and check_password_hash(user.password ,form.password.data): 
-             login_user(user) 
-             return redirect(url_for('admin')) 
-
-     return render_template('login.html' ,form=form )
-
-@app.route('/logout') 
-def logout(): 
-     return redirect(url_for('login'))
-
-
-
 if __name__ == '__main__':
     with app.app_context():  # Создаем контекст приложения
         db.create_all()
